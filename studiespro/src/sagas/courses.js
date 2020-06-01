aa5ff732c83864e866c474a741e9b14690b55739
CamilaGO <gon18398@uvg.edu.gt>
2020-06-01 12:12:12 -0500

    Ya borra de una todo

--- studiespro/src/sagas/courses.js
+++ studiespro/src/sagas/courses.js
@@ -38,7 +38,7 @@ import {
           }
         );
   
-        if (response.status === 204) {
+        if (response.status === 200) {
           const jsonResult = yield response.json();
           const {
             entities: { courses },
@@ -144,7 +144,7 @@ import {
           }
         );
   
-        if (response.status === 200) {
+        if (response.status === 204) {
           yield put(actions.completeRemovingCourse());
           console.log("Entro 204");
           // const {
